a159055c943b2776520e1a823bb7d26fcc7fa70d
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-03-29 15:00:08 +0530

    Created latitude thrower functionality

--- src/app.js
+++ src/app.js
@@ -15,19 +15,34 @@ import ReactDOM from "react-dom/client";
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { akhharekha: null }
+        this.state = { akhharekha: null, error: null, msg: null, displaytext: "loading" }
 
         window.navigator.geolocation.getCurrentPosition(
-            (position) => this.setState({ akhharekha: position.coords.latitude }),
-            () => this.setState({ akhharekha: "Couldn't fetch location" })
+            (position) => {
+                this.setState({ msg: "latitude:", akhharekha: position.coords.latitude })
+            },
+            (err) => this.setState({ msg: "error:", error: err.message })
         )
+
+
+
     }
 
+
+
     render() {
+
+        if (this.state.akhharekha == null && this.state.error !== null) {
+            this.setState({ displaytext: this.state.error })
+        } else if (this.state.error == null && this.state.akhharekha !== null) {
+            this.setState({ displaytext: this.state.akhharekha })
+        }
+
         return (
             <div>
-                Latitude: {this.state.akhharekha}
-            </div>
+                {/* {this.state.msg} {this.state.akhharekha} {this.state.error} */}
+                <h3>{this.state.msg}{this.state.displaytext}</h3>
+            </div >
         )
     }
 }
