4e50864a6022805fcfa9dd9b2049c4028921eef9
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-03-31 23:27:33 +0530

    Converted Season Display functionality to object based data-loading from functional data-loading

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -2,34 +2,49 @@ import React from 'react';
 import "./SeasonDisplay.css";
 import "semantic-ui-css/semantic.min.css";
 
+const seasonConfig = {
+    summer: {
+        text: "whoa, it's really hot, let's hit the beach!!",
+        iconName: "sun"
+    },
+    winter: {
+        text: "Burr, it's cold, turn on the heaters!!",
+        iconName: "snowflakes"
+    }
+}
+
 const getSeason = (lat, month) => {
     if (month >= 3 && month < 9) {
-        return lat > 0 ? "Summer" : "Winter";
+        return lat > 0 ? "summer" : "winter";
     } else {
-        return lat < 0 ? "Summer" : "Winter";
+        return lat < 0 ? "summer" : "winter";
     }
 
 }
 
 const SeasonDisplay = (props) => {
     const season = getSeason(props.res, new Date().getMonth());
-    let text = "";
-    let icon = "";
-    const seasonIcon = () => {
-        if (season == "Winter") {
-            text = "burrr! it's chilly";
-            icon = "snowflake";
-        } else {
-            text = "whoa, lets hit the beach!!";
-            icon = "sun";
-        }
-    }
-    seasonIcon();
+    console.log(season)
+    // let text = "";
+    // let icon = "";
+    // const seasonIcon = () => {
+    //     if (season == "Winter") {
+    //         text = "burrr! it's chilly";
+    //         icon = "snowflake";
+    //     } else {
+    //         text = "whoa, lets hit the beach!!";
+    //         icon = "sun";
+    //     }
+    // }
+    // seasonIcon();
+
+    const { text, iconName } = seasonConfig[season];
+
     return (
         <div id="container">
+            <i className={`${iconName} icon`} />
             <h2 id="responce">{text}</h2>
-            <i className={`${icon} icon`} />
-            <i className={`${icon} icon`} />
+            <i className={`${iconName} icon`} />
         </div>
     )
 }
