71ac093550dbc39de2eac90111ef37040a794d73
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-03-31 23:05:21 +0530

    Added icons and changed responces a lit bit

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,20 +1,35 @@
-import React from 'react'
-import "./SeasonDisplay.css"
+import React from 'react';
+import "./SeasonDisplay.css";
+import "semantic-ui-css/semantic.min.css";
 
 const getSeason = (lat, month) => {
     if (month >= 3 && month < 9) {
-        return lat > 0 ? "You are in Summer" : "You are in Winter";
+        return lat > 0 ? "Summer" : "Winter";
     } else {
-        return lat < 0 ? "You are in Summer" : "You are in Winter";
+        return lat < 0 ? "Summer" : "Winter";
     }
 
 }
 
 const SeasonDisplay = (props) => {
-    const season = getSeason(props.res, new Date().getMonth())
+    const season = getSeason(props.res, new Date().getMonth());
+    let text = "";
+    let icon = "";
+    const seasonIcon = () => {
+        if (season == "Winter") {
+            text = "burrr! it's chilly";
+            icon = "snowflake";
+        } else {
+            text = "whoa, lets hit the beach!!";
+            icon = "sun";
+        }
+    }
+    seasonIcon();
     return (
         <div id="container">
-            <h2 id="responce">{season}</h2>
+            <h2 id="responce">{text}</h2>
+            <i className={`${icon} icon`} />
+            <i className={`${icon} icon`} />
         </div>
     )
 }
