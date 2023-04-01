30235e1c7d4ab6381bbbf244c03b06534f4d218c
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-04-01 22:19:24 +0530

    added a default props for loading screen

--- src/spinner.js
+++ src/spinner.js
@@ -5,12 +5,16 @@ import "semantic-ui-css/semantic.min.css";
 const Spinner = (props) => {
     return (
         <div>
-            <div class="ui active dimmer">
-                <div class="ui text loader">{props.spinnerText}</div>
+            <div className="ui active dimmer">
+                <div className="ui text loader">{props.spinnerText}</div>
             </div>
             <p>{props.spinnerText}</p>
         </div>
     )
+}
+
+Spinner.defaultProps = {
+    spinnerText: "Loading..."
 }
 
 export default Spinner;
